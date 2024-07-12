55efca9bdf4a4cf4c949ac5d52c642b34c23e4f5
Nagaraj <nagarajmb235@gmail.com>
2024-07-12 12:57:53 +0530

    add refreshAccessToken endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -139,7 +139,7 @@ const loginUser = asyncHandler( async (req,res)=>{
 
   const { email, username , password } = req.body
 
-  if(!email || !username){
+  if(!(email || username)){
     throw new ApiError(400 ,"  username or email is required ")
   }
 
